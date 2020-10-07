ee5fad03308f6be20fc77044a6b099680b25e5a3
Victoria_Cardwell <victoria.cardwell1@gmail.com>
2020-10-07 11:45:18 -0400

    Adding Game Components to JS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 const messageBox = document.querySelector("#message");
 
 let currentPlayer = "X"
+let gameInput = ["", "", "", "", "","", "", "", ""]
 
+const weHaveAWinner = () => `${currentPlayer} won!`
+const weHaveADraw = () => "It's a Draw! No Winner!"
 const playerAlert = () => `${currentPlayer}, it's your turn!`
 
-messageBox.innerHTML = playerAlert();
-console.log(messageBox);
+const winningLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+
+
+//console.log(messageBox);
